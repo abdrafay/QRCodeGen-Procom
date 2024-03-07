ddd296f22138abf1935f7c89fb168bbd19a9117e
abdrafay <zainrafay12@gmail.com>
2024-03-07 09:21:32 +0500

    Register Payhabib Done

---
+++ frontend/app/merchant/(auth)/layout.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Image from 'next/image'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+
+interface AuthLayoutProps {
+    children: React.ReactNode
+}
+
+const AuthLayout = ({children}: AuthLayoutProps) => {
+    const images = [
+        '/bar.png',
+        '/line.png'
+    ]
+    return (
+        <div className='flex items-center justify-center w-screen h-screen p-5'>
+            <div className="flex w-full md:w-10/12 max-w-[1000px] h-[90vh] border-2 rounded-lg p-2">
+                <div className='w-full md:w-1/2'>
+                    {children}
+                </div>
+                <div className='hidden md:block w-full md:w-1/2 rounded-lg bg-purple-600 overflow-hidden relative'>
+                    
+                    <div className='absolute w-[200px] h-[200px] transform rounded-xl rotate-[14deg] -bottom-[5rem] -left-32 bg-purple-500'></div>
+                    <div className='absolute w-[200px] h-[200px] transform rounded-xl rotate-[14deg] -top-[5rem] -right-20 bg-purple-500'></div>
+
+                    <div className="flex w-full h-full justify-center items-center p-10">
+                        <Carousel>
+                            <CarouselContent>
+                                {images.map((image, ind) => (
+                                    <CarouselItem key={ind}>
+                                        <div className='text-center'>
+                                            <Image src={image} className='max-w-full mx-auto' width={300} height={200} alt="Charts"/>                      
+                                            <h2 className="text-2xl text-white font-bold text-center my-3">Visualize payment statistics</h2>
+                                            <p className="text-xs text-center text-white">These visuals are used to analyze and understand various aspects of payment activity, trends, and patterns</p>
+                                        </div>
+                                    </CarouselItem>
+                                ))}
+                                
+                            </CarouselContent>
+                            <div className='absolute left-1/2 mt-10'>
+                                <CarouselPrevious />
+                                <CarouselNext />
+                            </div>
+                        </Carousel>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AuthLayout
---
+++ frontend/app/merchant/(auth)/register/form/index.tsx
@@ -0,0 +1,166 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+// hooks
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Control, useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { getSession, register } from '@/lib'
+import { useRouter } from "next/navigation";
+import { UserInterface } from '@/loginInterface'
+
+
+// UI elements
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    accountNo: z.string().min(2, {
+        message: "Account No must be at least 2 characters.",
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address.",
+    }),
+    phoneNo: z.string().min(10, {
+        message: "Please enter a valid phone number.",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters.",
+    }),
+
+})
+
+const defaultValues = {
+    username: "",
+    accountno: "",
+    email: "",
+    phone: "",
+    password: "",
+}
+
+const LoginForm = () => {
+    const router = useRouter()
+    const [buttonLoading, setButtonLoading] = useState<boolean>(false)
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: defaultValues,
+    })
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        // login(values as LoginInterface)
+        const AllValues = {
+            ...values,
+            role: 'merchant'
+        }
+        setButtonLoading(true)
+        const registered = await register(AllValues as UserInterface)
+        setButtonLoading(false)
+        if(registered) {
+            router.push('/merchant')
+        }
+    }
+    useEffect(() => {
+        const run = async () => {
+            const session = await getSession()
+            if(session) {
+                router.push('/customer')
+            }
+        }
+        run()
+    },[])
+
+    return (
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 my-5">
+           
+            <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input {...field} placeholder='Enter username'/>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+            control={form.control}
+            name="accountNo"
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Account No</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder='Enter account no'/>
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                        <Input {...field} type="email" placeholder='Enter email'/>
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name='phoneNo'
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Phone Number</FormLabel>
+                    <FormControl>
+                        <Input {...field} type="tel" placeholder='Enter email'/>
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                        <Input {...field} type="password" placeholder='Enter password'/>
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <div className='text-center'>
+                <Button type="submit" disabled={buttonLoading} className='bg-purple-500 w-full my-2'>
+                {buttonLoading ? 'Submitting..' : 'Submit' }
+                    
+                </Button>
+            </div>
+        </form>
+    </Form>
+    )
+}
+
+export default LoginForm
+
